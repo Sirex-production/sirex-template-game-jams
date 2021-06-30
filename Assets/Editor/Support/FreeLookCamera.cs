ba7681de30665fd269e3a022d4ed71102655dc2e
Vo1z <vitya.voody@gmail.com>
2021-06-30 20:21:36 +0200

    dev 0
    -Testing environment was set up
    -MonoSingleton class was added (tested)

--- Assets/Editor/Support/FreeLookCamera.cs
+++ Assets/Editor/Support/FreeLookCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 
 using UnityEngine;
 
-namespace UnityTemplateProjects
+namespace Support
 {
     public class FreeLookCamera : MonoBehaviour
     {
---
+++ Assets/Editor/Support/MonoSingleton.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Support
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        public static T Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = this as T;
+        }
+    }
+}
