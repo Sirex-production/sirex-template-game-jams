bf51dc2ca7fe8ed62715b29e9e5589659eb86152
Vo1z <vitya.voody@gmail.com>
2021-12-23 16:30:24 +0200

    dev 39
    -VFXController was introduced (issue #45)
    -Manual tests for VFXController was added
    -Post processing was set up
    -Documentation for VFXController was added
    -GameManagement prefab was extended with VFXController

---
+++ Assets/Tests/ManualTesting/VFXControllerTest.cs
@@ -0,0 +1,26 @@
+using NaughtyAttributes;
+using Support;
+using UnityEngine;
+
+namespace Tests.Manual
+{
+    public class VFXControllerTest : MonoBehaviour
+    {
+        [SerializeField] private string volumeId;
+        [SerializeField] private float lerpSpeed = 1;
+        
+        [Button("Change volume without lerp")]
+        public void ChangeVolumeWithOutLerp()
+        {
+            var randomWeight = Random.Range(0f, 1f);
+            VFXController.Instance.ChangePostProcessingVolume(volumeId, randomWeight);
+        }
+        
+        [Button("Change volume with lerp")]
+        public void ChangeVolumeWithLerp()
+        {
+            var randomWeight = Random.Range(0f, 1f);
+            VFXController.Instance.ChangePostProcessingVolume(volumeId, randomWeight, lerpSpeed);
+        }
+    }
+}
